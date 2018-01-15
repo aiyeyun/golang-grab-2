cd5ce58a78e22845d22437187c6ea5e33fa2276b
夜云 <644362887@qq.com>
2018-01-15 10:50:15 +0800

    天津

--- core/algorithm/shishicai/play1/play.go
+++ core/algorithm/shishicai/play1/play.go
@@ -2,13 +2,13 @@ package play1
 
 import (
 	"fmt"
-	"xmn_2/core/model"
+	"xmn/core/model"
 	"time"
 	"log"
 	"strings"
-	"xmn_2/core/logger"
+	"xmn/core/logger"
 	"strconv"
-	"xmn_2/core/mail"
+	"xmn/core/mail"
 )
 
 //计算分析结构体
@@ -33,12 +33,16 @@ func Calculation()  {
 	xjscc := new(model.Xjssc)
 	xjCodes = xjscc.Query("200")
 
+	tjssc := new(model.Tjssc)
+	tjCodes = tjssc.Query("200")
+
 	//获取数据包
 	cPackage := new(model.Play1)
 	configPackage := cPackage.Query()
 
 	cq_q3s, cq_z3s, cq_h3s := getFrontCenterAfterCodes(cqsscType)
 	xj_q3s, xj_z3s, xj_h3s := getFrontCenterAfterCodes(xjsscType)
+	tj_q3s, tj_z3s, tj_h3s := getFrontCenterAfterCodes(tjsscType)
 
 	allCodes := &allCpCodes{
 		cq_q3s: cq_q3s,
@@ -48,6 +52,10 @@ func Calculation()  {
 		xj_q3s: xj_q3s,
 		xj_z3s: xj_z3s,
 		xj_h3s: xj_h3s,
+
+		tj_q3s: tj_q3s,
+		tj_z3s: tj_z3s,
+		tj_h3s: tj_h3s,
 	}
 
 	for i := range configPackage {
@@ -146,6 +154,39 @@ func analysis(packet *model.Play1, allCodes *allCpCodes)  {
 		packet: packet,
 	}
 
+	//天津前3
+	tj_q3 := &computing{
+		packet_a_map: dataTxtMapPackageA,
+		packet_b_map: dataTxtMapPackageB,
+		code: allCodes.tj_q3s,
+		cpType: tjsscType,
+		cpTypeName: cpTypeName[tjsscType],
+		position: "前3",
+		packet: packet,
+	}
+
+	//天津中3
+	tj_z3 := &computing{
+		packet_a_map: dataTxtMapPackageA,
+		packet_b_map: dataTxtMapPackageB,
+		code: allCodes.tj_z3s,
+		cpType: tjsscType,
+		cpTypeName: cpTypeName[tjsscType],
+		position: "中3",
+		packet: packet,
+	}
+
+	//天津后3
+	tj_h3 := &computing{
+		packet_a_map: dataTxtMapPackageA,
+		packet_b_map: dataTxtMapPackageB,
+		code: allCodes.tj_h3s,
+		cpType: tjsscType,
+		cpTypeName: cpTypeName[tjsscType],
+		position: "后3",
+		packet: packet,
+	}
+
 	go cq_q3.calculate()
 	go cq_z3.calculate()
 	go cq_h3.calculate()
@@ -153,6 +194,10 @@ func analysis(packet *model.Play1, allCodes *allCpCodes)  {
 	go xj_q3.calculate()
 	go xj_z3.calculate()
 	go xj_h3.calculate()
+
+	go tj_q3.calculate()
+	go tj_z3.calculate()
+	go tj_h3.calculate()
 }
 
 //计算分析
@@ -161,16 +206,21 @@ func (c *computing) calculate()  {
 		//fmt.Println("a出现几期的b", c.packet.Alias, c.cpTypeName , c.position , "等待出现最新的号码")
 		return
 	}
-	
-	// 循环一次后 是否出现a 包
+
+	// 循环的上一期 是否出现a包
 	pre_a_show := false
-	// 循环一次后 是否出现c 包
+
+	// 循环的上一期 是否出现b包
 	pre_b_show := false
-	// 循环一次后 是否出现c 包
+
+	// 循环的上一期 是否出现c包
 	pre_c_show := false
 
-	// 连续出现自定义几期b 计数器
-	continuity_b_number := 0
+	// a 包出现后才能计算
+	a_show := false
+
+	// a 包出现后 几期了 还没出现a包
+	a_show_after_num := 0
 
 	// 报警期数 计数器
 	number := 0
@@ -188,31 +238,39 @@ func (c *computing) calculate()  {
 			in_c_package = true
 		}
 
-
-		// 上一期 出现a包 并且 本期 出现了b包 或 本期出现了c包
-		if pre_a_show && (in_b_package || in_c_package) {
-			continuity_b_number += 1
+		// a包出现后 已经有n期没出现a包了
+		if a_show && !in_a_package {
+			a_show_after_num += 1
 		}
 
-		// 上一期出现c包 或者 上一期出现b 包 并且 本期出现c包 或者 本期出现 b包
-		if (pre_c_show || pre_b_show) && (in_c_package || in_b_package) {
-			continuity_b_number += 1
+		// a包出现后 还未等到 重新等待一下a包
+		if a_show && in_a_package {
+			// 本期不做任何计算
+			a_show = false
+			// a包出现后 开了几期bc包了 清零 因为又出现a包了
+			a_show_after_num = 0
 		}
 
 		// 累加连续b 已经达到 了自定义几b 值 报警期数 + 1
-		if continuity_b_number == c.packet.ContinuityNumber {
+		if a_show_after_num == c.packet.ContinuityNumber {
 			number += 1
 		}
 
-		// 设置自定义连续几B, 开奖超出自定义连续几B 就算开奖 清零 并且本期 未出现c包 只有b包才能清零
-		if continuity_b_number > c.packet.ContinuityNumber && !in_c_package {
-			// 连续b 清零
-			continuity_b_number = 0
-			// 报警期数 清零
-			number = 0
+		// 当到了 设置到连续n b后的再一期 就要等到下一个a出现再计算
+		if a_show_after_num == c.packet.ContinuityNumber + 1 {
+			// 本期包含b包 算开 一切清零
+			if in_b_package {
+				number = 0
+			}
+
+			// 等待下一个a包出现
+			a_show = false
+			// a包出现后已经连续几次出bc包了 清零
+			a_show_after_num = 0
 		}
 
 		strHtmlLog += "<div>开奖号: </div>" + c.code[i] + "<br/>"
+		strHtmlLog += "<div>a出现后 几期了: " + strconv.Itoa(a_show_after_num)+ "</div>"
 		strHtmlLog += "<div>上一期包含A包吗: " + strconv.FormatBool(pre_a_show) + "</div>"
 		strHtmlLog += "<div>上一期包含B包吗: " + strconv.FormatBool(pre_b_show) + "</div>"
 		strHtmlLog += "<div>上一期包含C包吗: " + strconv.FormatBool(pre_c_show) + "</div>"
@@ -220,27 +278,36 @@ func (c *computing) calculate()  {
 		strHtmlLog += "<div>本期包含A包吗: " + strconv.FormatBool(in_a_package) + "</div>"
 		strHtmlLog += "<div>本期包含B包吗: " + strconv.FormatBool(in_b_package) + "</div>"
 		strHtmlLog += "<div>本期包含C包吗: " + strconv.FormatBool(in_c_package) + "</div>"
-		strHtmlLog += "<div>设置的连续 " + strconv.Itoa(c.packet.ContinuityNumber) + " B 已经累加到 " + strconv.Itoa(continuity_b_number) + "</div>"
+		strHtmlLog += "<div>设置的连续 " + strconv.Itoa(c.packet.ContinuityNumber) + " B 已经累加到 " + strconv.Itoa(a_show_after_num) + "</div>"
 		strHtmlLog += "<div>报警期数累加到 " + strconv.Itoa(number) + "</div><br/>"
 
-		// 本次循环执行完后 再更新 本期是否出现了a 包
+		// 本期包含a包 a包允许计算开始
 		if in_a_package {
+			a_show = true
+			a_show_after_num = 0
 			pre_a_show = true
+		}else {
+			pre_a_show = false
 		}
+
 
 		// 本次循环执行完后 再更新 本期是否出现了b 包
 		if in_b_package {
 			pre_b_show = true
+		}else {
+			pre_b_show = false
 		}
 
 		// 本次循环执行完后 再更新 本期是否出现了c 包
 		if in_c_package {
 			pre_c_show = true
+		}else {
+			pre_c_show = false
 		}
 	}
 
 	// 最新的一期 包含 c 包 或者包含 b包 并且 达到报警期数
-	if (pre_b_show || pre_c_show) && number >= c.packet.Number {
+	if a_show && (pre_b_show || pre_c_show) && number >= c.packet.Number {
 		emailTitle := "<div>a出现几期的b 设置的连续"+ strconv.Itoa(c.packet.ContinuityNumber) +"B 报警"+ " 彩种: "+ c.cpTypeName + " 位置: "+ c.position + " 包别名: "+ c.packet.Alias +" 报警 ["+ strconv.Itoa(number) +"]期 提示</div> <br/><br/>"
 		emailTitle += strHtmlLog
 		go mail.SendMail(c.cpTypeName + "a出现几期的b", emailTitle)
@@ -315,6 +382,16 @@ func isRepeat(cpType int) bool {
 		}
 		index := len(xjCodes) - 1
 		newCode = xjCodes[index].One + xjCodes[index].Two + xjCodes[index].Three + xjCodes[index].Four + xjCodes[index].Five
+	}
+
+	// 天津时时彩
+	if cpType == tjsscType {
+		//获取本次查询的最新号码
+		if len(tjCodes) == 0 {
+			return false
+		}
+		index := len(tjCodes) - 1
+		newCode = tjCodes[index].One + tjCodes[index].Two + tjCodes[index].Three + tjCodes[index].Four + tjCodes[index].Five
 	}
 
 	//获取内存中最新的重新开奖号码
